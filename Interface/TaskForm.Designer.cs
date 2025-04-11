787ca729a8e73e9109e2f76c9f18d26a7cd1a2ca
Nikita Sergeevich <niba2017@mail.ru>
2025-04-11 11:42:09 +0300

    вывод ошибок

--- Interface/TaskForm.Designer.cs
+++ Interface/TaskForm.Designer.cs
@@ -40,18 +40,19 @@
             // 
             Title.AutoSize = true;
             Title.Font = new Font("Monotype Corsiva", 36F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            Title.Location = new Point(369, 21);
+            Title.Location = new Point(422, 28);
             Title.Name = "Title";
-            Title.Size = new Size(126, 57);
+            Title.Size = new Size(154, 72);
             Title.TabIndex = 0;
             Title.Text = "label1";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Monotype Corsiva", 24F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            textBox1.Location = new Point(241, 160);
+            textBox1.Location = new Point(275, 213);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(384, 43);
+            textBox1.Size = new Size(438, 52);
             textBox1.TabIndex = 1;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
@@ -59,18 +60,19 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Monotype Corsiva", 24F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            label2.Location = new Point(232, 118);
+            label2.Location = new Point(265, 157);
             label2.Name = "label2";
-            label2.Size = new Size(404, 39);
+            label2.Size = new Size(506, 49);
             label2.TabIndex = 3;
             label2.Text = "Введите массив для сортировки";
             // 
             // button1
             // 
             button1.Font = new Font("Monotype Corsiva", 18F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            button1.Location = new Point(349, 222);
+            button1.Location = new Point(399, 296);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(168, 49);
+            button1.Size = new Size(192, 65);
             button1.TabIndex = 5;
             button1.Text = "Сортировать";
             button1.UseVisualStyleBackColor = true;
@@ -80,32 +82,35 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Monotype Corsiva", 24F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            label3.Location = new Point(257, 279);
+            label3.Location = new Point(294, 372);
             label3.Name = "label3";
-            label3.Size = new Size(335, 39);
+            label3.Size = new Size(418, 49);
             label3.TabIndex = 6;
             label3.Text = "Отсортированный массив";
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Monotype Corsiva", 24F, FontStyle.Italic, GraphicsUnit.Point, 204);
-            textBox2.Location = new Point(241, 321);
+            textBox2.Location = new Point(275, 428);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(384, 43);
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(438, 52);
             textBox2.TabIndex = 7;
             // 
             // TaskForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(199, 210, 242);
-            ClientSize = new Size(855, 482);
+            ClientSize = new Size(977, 643);
             Controls.Add(textBox2);
             Controls.Add(label3);
             Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(Title);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "TaskForm";
             Text = "TaskForm";
             ResumeLayout(false);
